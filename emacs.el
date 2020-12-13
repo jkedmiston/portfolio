f7f03659409b6cebc121aae79465631a2f6b32e1
John Edmiston <johnkedmiston@gmail.com>
2020-12-13 12:24:58 -0800

    adding emacs example

---
+++ emacs.el
@@ -0,0 +1,45 @@
+;; init.el --- Emacs configuration
+
+;; INSTALL PACKAGES
+;; --------------------------------------
+;; REFS:
+;; https://east.fm/posts/configuring-gnu-emacs-with-elpy-on-macos/index.html#
+;; https://github.com/porterjamesj/virtualenvwrapper.el
+;; https://www.reddit.com/r/emacs/comments/i4886k/trouble_setting_up_elpy_part_2_total_noob_please/
+;; https://realpython.com/emacs-the-best-python-editor/
+;; key command was (setq elpy-rpc-virtualenv-path 'current) to silenve venv errors
+;; and then (pyvenv-activate "/Users/johnedmiston/anaconda3/envs/myco") to point at the conda env
+
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq inhibit-startup-message t) ;; hide the startup message
+;;(custom-set-variables
+;; (pyvenv-activate "/Users/johnedmiston/anaconda3/envs/myco")
+;; https://github.com/jorgenschaefer/elpy
+(package-initialize)
+(elpy-enable)
+(setq elpy-rpc-virtualenv-path 'current)
+(require 'virtualenvwrapper)
+(venv-initialize-interactive-shells) ;; if you want interactive shell support
+(venv-initialize-eshell) ;; if you want eshell support
+;; note that setting `venv-location` is not necessary if you
+;; use the default location (`~/.virtualenvs`), or if the
+;; the environment variable `WORKON_HOME` points to the right place
+(setq venv-location "/Users/johnedmiston/anaconda3/envs/myco")
+
+(pyvenv-activate "/Users/johnedmiston/anaconda3/envs/myco")
+;; Enable Flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; Enable autopep8
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+
+(global-set-key (kbd "\C-c l") 'goto-line)
+(global-set-key "\M-\r" 'shell-resync-dirs)
