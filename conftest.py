da15f3607024a3e9eadd26b581541925a2280a38
John Edmiston <johnkedmiston@gmail.com>
2021-02-21 00:22:52 -0800

    [pubsub-send-recv] add pubsub comm interchange

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pytest
 import tempfile
+import json
 
 
 @pytest.fixture
@@ -13,6 +14,7 @@ def app(monkeypatch):
 
     database_url = 'sqlite:///%s' % database
     os.environ["DATABASE_URL"] = database_url
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = """{  "type": "service_account",  "project_id": "testproject",  "private_key_id": "f1", "private_key": "f2", "client_email": "na", "client_id": "a", "auth_uri": "a"}"""
     app = create_app()
     with app.app_context():
         db.create_all()
@@ -20,6 +22,13 @@ def app(monkeypatch):
     # See https://flask.palletsprojects.com/en/1.1.x/testing/
     os.close(db_fd)
     os.unlink(database)
+
+
+@pytest.fixture
+def service_env(app):
+    service_account_info = json.loads(
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+    return service_account_info
 
 
 def pytest_configure(config):
