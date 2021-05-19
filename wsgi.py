4d10242a528f6aae9e0aca393409c42eae4f76b7
John Edmiston <johnkedmiston@gmail.com>
2021-05-18 17:51:58 -0700

    clean docstring

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,6 @@
 from main import create_app
 
 
-# The application was initially being initialized implicitly when this script
-# was imported and that was causing problems in the test environment.
-# (SQLALCHEMY_DATABASE_URI was never reset by test harness.)
 def app():
     return create_app()
 
