307fbf1292cab698569f46798d9eb8df60fe2019
John Edmiston <johnkedmiston@gmail.com>
2024-06-02 20:18:20 -0700

    Reset device

--- hardware/pubsub_depth_cam_pull.py
+++ hardware/pubsub_depth_cam_pull.py
@@ -4,6 +4,7 @@ Pulls messages from the channel to trigger a depth camera event
 
 import os
 import json
+import time
 import subprocess
 from subprocess import TimeoutExpired
 from google.cloud import pubsub_v1
@@ -39,6 +40,22 @@ def reset_camera():
     return
 
 
+def reset_camera1():
+    """
+    USB port reset. 
+    The realsense cam is finicky and not reliable on reuse. This resets the USB. Functionality from https://askubuntu.com/questions/645/how-do-you-reset-a-usb-device-from-the-command-line
+    """
+    cmd = 'python reset_device_alt.py'
+    proc = subprocess.Popen([cmd],
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    print("stdout %s" % stdout)
+    print("stdout %s" % stderr)
+    return
+
+
 def callback(message):
     """
     Take the triggered photo 
@@ -47,7 +64,7 @@ def callback(message):
     data = json.loads(message.data.decode('utf-8'))
     publish_time = message.publish_time
 
-    reset_camera()
+    reset_camera1()
     ntrials = 10
     for j in range(ntrials):
         proc = subprocess.Popen(["run_d435"],
@@ -80,11 +97,15 @@ def callback(message):
 
             return
         except TimeoutExpired:
+            print("timeout expired")
             proc.kill()
-            stdout, stderr = proc.communicate()
+            # stdout, stderr = proc.communicate()
             reset_camera()
+            time.sleep(1)
 
-    print("stdout", stdout, stderr)
+    publish_message(
+        topic_name=os.environ["DEPTH_CAM_TO_SERVER_TOPIC"],
+        data={})
     raise Exception("Proc communication error %s %s" % (stdout, stderr))
 
 
@@ -95,7 +116,7 @@ while True:
 
     streaming_pull_future = subscriber.subscribe(
         subscription_path, callback=callback)
-    print(f"Listening for messages on {subscription_path}..\n")
+    # print(f"Listening for messages on {subscription_path}..\n")
 
     # Wrap subscriber in a 'with' block to automatically call close() when done.
     with subscriber:
@@ -107,4 +128,4 @@ while True:
         except TimeoutError:
             streaming_pull_future.cancel()
 
-        print("looping")
+        # print("looping")
